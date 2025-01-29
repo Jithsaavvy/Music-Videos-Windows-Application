9068ed55f490d00fe9e04db99f13a8f3824278fb
Jithin Sasikumar <jithsasikumar@gmail.com>
2025-01-29 19:15:09 +0100

    Cleaned up the repo

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Universal Media Player
-Universal Media Player is a win32 application that was developed and deployed using C#, XAML as part of .Net Framework. It performs as a real time music or video player application with some advanced features like drag and drop , casting to other devices etc. 
+Universal Media Player is a cross-platform Windows media player application that was developed and deployed using C# and XAML as part of the .Net Framework. It supports the deployment across Windows PCs, tablets and phones. It performs real-time media streaming with features like drag-and-drop and device casting.
---
+++ universal-media-player/universal-media-player/models/Videoprop.cs
@@ -0,0 +1,28 @@
+﻿// Class to store the video properities
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace Music
+{
+    public class Videoprop
+    {
+        public int ID { get; set; }
+        public string videotitle { get; set; }
+        public string videoartist { get; set; }
+        public string videoalbum { get; set; }
+        public string videoname { get; set; }
+        public bool nextvideo { get; set; }
+        public StorageFile Name { get; set; }
+        public StorageFile videofile { get; set; }
+        public BitmapImage albumcover;
+        public StorageFolder folder { get; set; }
+        public bool selected { get; set; }
+    }
+
+}
